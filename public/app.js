eb5cbf17869d011f543297661df9a332391ac622
Rafez <Rafez@bdomain.com>
2021-12-22 14:58:04 +0600

    added image with the blog post

--- public/app.js
+++ public/app.js
@@ -2,19 +2,17 @@
 const blogForm = document.querySelector(".blog_form");
 const blogTitle = document.querySelector("#blog_title");
 const blogContent = document.querySelector("#blog_content");
-const blogFormSubmitButton = document.querySelector("#blog_form_button");
 const blogList = document.querySelector(".blog_list");
 blogForm.addEventListener("submit", (event) => {
     event.preventDefault();
     console.log(blogTitle.value, blogContent.value);
     const blogListItem = document.createElement("li");
     const div = document.createElement("div");
-    const blogTitleElement = document.createElement("p");
-    const blogContentElement = document.createElement("p");
-    blogTitleElement.textContent = blogTitle.value;
-    blogContent.textContent = blogContent.value;
-    div.appendChild(blogTitleElement);
-    div.appendChild(blogContentElement);
+    div.innerHTML = `
+    <h2>${blogTitle.value}</h2>
+    <p>${blogContent.value}</p>
+    <img src = "./images/DSC06018.jpg" alt="ABC" />
+    `;
     blogListItem.appendChild(div);
     blogList.appendChild(blogListItem);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,25 @@
 const blogForm = document.querySelector(".blog_form") as HTMLFormElement;
 const blogTitle = document.querySelector("#blog_title") as HTMLInputElement;
-const blogContent = document.querySelector("#blog_content") as HTMLInputElement;
-const blogFormSubmitButton = document.querySelector("#blog_form_button") as HTMLButtonElement;
+const blogContent = document.querySelector("#blog_content") as HTMLTextAreaElement;
+
 
 const blogList = document.querySelector(".blog_list") as HTMLUListElement;
 
 blogForm.addEventListener("submit", (event: Event) => {
 
     event.preventDefault();
-
     console.log(blogTitle.value, blogContent.value);
 
     const blogListItem: HTMLLIElement = document.createElement("li");
     const div: HTMLDivElement = document.createElement("div");
-    const blogTitleElement: HTMLParagraphElement = document.createElement("p");
-    const blogContentElement: HTMLParagraphElement = document.createElement("p");
 
-    blogTitleElement.textContent = blogTitle.value;
-    blogContent.textContent = blogContent.value;
+    div.innerHTML = `
+    <h2>${blogTitle.value}</h2>
+    <p>${blogContent.value}</p>
+    <img src = "./images/DSC06018.jpg" alt="ABC" />
+    `;
 
-    div.appendChild(blogTitleElement);
-    div.appendChild(blogContentElement);
     blogListItem.appendChild(div);
     blogList.appendChild(blogListItem);
-
 
 })
